f61400d718d894a5cd5bc256aeb1c2c9a744cd43
AutumnCTH <3465853848@qq.com>
2026-03-18 11:36:22 +0800

    build-3

--- Media Convertor with GUI.py
+++ Media Convertor with GUI.py
@@ -1,9 +1,15 @@
 import os
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import filedialog
+from tkinter import messagebox
 
 def importFile():
-    pass
+    global pathImport
+    global MainWindow
+    pathImport = filedialog.askopenfilename()
+    Label(MainWindow, text=fr"{pathImport}").grid(row=0, column=2, sticky=W)
+
 
 def saveFile():
     pass
@@ -13,36 +19,39 @@ def convert():
 
 def main():
 
-    MCG_VERSION = "build-2"
-    FFMPEG_PATH = ".\\ffmpeg\\bin\\ffmpeg.exe"
+    MCG_VERSION = "build"
+    PATH_FFMPEG = ".\\ffmpeg\\bin\\ffmpeg.exe"
+    pathImport = NONE
+    pathExport = NONE
 
-    mainWindow = Tk()
-    mainWindow.title("MCG " + MCG_VERSION)
+    MainWindow = Tk()
+    MainWindow.title("MCG " + MCG_VERSION)
 
-    Label(mainWindow, text="Import File:").grid(row=0, sticky=W)
-    Label(mainWindow, text="Export File:").grid(row=1, sticky=W)
-    Button(mainWindow, text="Choose File", command=importFile).grid(
+    Label(MainWindow, text="Import File:").grid(row=0, sticky=W)
+    Label(MainWindow, text="Export File:").grid(row=1, sticky=W)
+    Button(MainWindow, text="Choose File", command=importFile).grid(
         row=0, column=1, sticky=W)
-    Button(mainWindow, text="Save as", command=saveFile).grid(
+    Button(MainWindow, text="Save as", command=saveFile).grid(
         row=1, column=1, sticky=W)
-    Label(mainWindow, text="Width(px):").grid(row=2, column=0, sticky=W)
-    Label(mainWindow, text="Height(px):").grid(row=2, column=2, sticky=W)
-    Label(mainWindow, text="Bitrate(kbps):").grid(row=2, column=4, sticky=W)
-    width_spinbox = Spinbox(mainWindow, from_=0, to=300000, width=5)
-    width_spinbox.grid(row=2, column=1, sticky=W)
+    Label(MainWindow, text="Width(px):").grid(row=2, column=0, sticky=W)
+    Label(MainWindow, text="Height(px):").grid(row=2, column=2, sticky=W)
+    Label(MainWindow, text="Bitrate(kbps):").grid(row=2, column=4, sticky=W)
 
-    height_spinbox = Spinbox(mainWindow, from_=0, to=300000, width=5)
-    height_spinbox.grid(row=2, column=3, sticky=W)
+    WidthSpinbox = Spinbox(MainWindow, from_=0, to=300000, width=5)
+    WidthSpinbox.grid(row=2, column=1, sticky=W)
 
-    bitrate_spinbox = Spinbox(mainWindow, from_=0, to=300000, width=5)
-    bitrate_spinbox.grid(row=2, column=5, sticky=W)
+    HeightSpinbox = Spinbox(MainWindow, from_=0, to=300000, width=5)
+    HeightSpinbox.grid(row=2, column=3, sticky=W)
 
-    Button(mainWindow, text="Start", command=convert).grid(sticky=E)
-    Button(mainWindow, text="Quit", command=quit).grid(sticky=E)
+    BitrateSpinbox = Spinbox(MainWindow, from_=0, to=300000, width=5)
+    BitrateSpinbox.grid(row=2, column=5, sticky=W)
+
+    Button(MainWindow, text="Start", command=convert).grid(sticky=E)
+    Button(MainWindow, text="Quit", command=quit).grid(sticky=E)
 
 
 
-    mainWindow.mainloop()
+    MainWindow.mainloop()
 
 if __name__ == "__main__":
     main()
