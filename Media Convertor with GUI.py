e32f7e4d09ef73a2ec726959a34154fe42175c96
AutumnCTH <3465853848@qq.com>
2026-03-18 10:42:01 +0800

    build-0

---
+++ Media Convertor with GUI.py
@@ -0,0 +1,17 @@
+import os
+from tkinter import *
+from tkinter.ttk import *
+
+
+def main():
+
+    MCG_VERSION = "build-0"
+
+    mainWindow = Tk()
+    mainWindow.title("MCG " + MCG_VERSION)
+
+
+    mainWindow.mainloop()
+
+if __name__ == "__main__":
+    main()
